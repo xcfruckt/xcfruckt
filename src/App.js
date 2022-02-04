7ecdd2fe9524d83d4ce34836ab8a4b2552f65f8f
xcfruckt <vk_sis@mail.ru>
2022-02-05 00:53:12 +0300

    yes

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, {useMemo, useState} from "react";
+import './styles/App.css'
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+import PostFilter from "./components/PostFilter";
+
+function App() {
+
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'А', body: 'В'},
+        {id: 2, title: 'Б', body: 'Б'},
+        {id: 3, title: 'В', body: 'А'},
+    ])
+
+    const [filter, setFilter] = useState({sort: '', query: ''})
+
+    const sortedPosts = useMemo(() => {
+        if (filter.sort) {
+            return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
+        }
+        return posts;
+    }, [filter.sort, posts])
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+    }, [filter.query, sortedPosts])
+
+
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+  return (
+    <div className="App">
+        <PostForm create={createPost}/>
+        <hr style={{margin:'15px 0'}}/>
+        <PostFilter filter={filter} setFilter={setFilter}/>
+
+        {sortedAndSearchedPosts.length
+            ?
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Список постов'}/>
+            :
+            <h1 style={{textAlign: 'center'}}>
+                Посты не найдены
+            </h1>
+        }
+
+    </div>
+  );
+}
+
+export default App;
